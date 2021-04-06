5d1af889279f81109cac0971a944147431c0e995
Fernando Cardo <fernandocardo@gmail.com>
2021-04-06 18:09:58 -0300

    feat: Inclusão do consumidor, para simular o tracing

---
+++ src/CRM.Client/Api/ClienteApi.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CRM.Client.Api
+{
+    public class ClienteApi
+    {
+
+        private readonly HttpClient _httpclient;
+        private readonly IConfiguration _configuration;
+        public ClienteApi(HttpClient client, IConfiguration configuration)
+        {
+            _httpclient = client;
+            _configuration = configuration;
+        }
+        public async Task<string> GetCliente()
+        {
+            var uri = new Uri(_configuration.GetValue<string>("ClientetURL"));
+            var response = await _httpclient.GetAsync(uri);
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                var contents = await response.Content.ReadAsStringAsync();
+
+
+                return contents;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+    }
+}
---
+++ src/CRM.Client/Startup.cs
@@ -0,0 +1,57 @@
+using CRM.Client.Api;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Client
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddHttpClient<ClienteApi>();
+
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
